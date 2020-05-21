77d88f2f5b8f3d94b3fdaecc91bc129b327c8602
trumanpurnell <truman.purnell@gmail.com>
2020-05-21 00:14:50 -0700

    converts to react app

---
+++ src/board.js
@@ -0,0 +1,115 @@
+import React from 'react'
+
+import './index.css'
+
+export default class ChessBoard extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <table className="chess-board">
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>a</th>
+                    <th>b</th>
+                    <th>c</th>
+                    <th>d</th>
+                    <th>e</th>
+                    <th>f</th>
+                    <th>g</th>
+                    <th>h</th>
+                </tr>
+                <tr>
+                    <th>8</th>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                </tr>
+                <tr>
+                    <th>7</th>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                </tr>
+                <tr>
+                    <th>6</th>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                </tr>
+                <tr>
+                    <th>5</th>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                </tr>
+                <tr>
+                    <th>4</th>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                </tr>
+            </tbody>
+        </table>
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ChessBoard from './board.js'
+
+ReactDOM.render(
+    <ChessBoard />,
+    document.getElementById('root')
+)
