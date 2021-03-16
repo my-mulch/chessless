88166b22c656bcc82aefb21f28e03b02c40e38de
trumanpurnell <truman.purnell@gmail.com>
2021-03-15 20:41:22 -0600

    we are kinda moving

--- view/components/App.jsx
+++ view/components/App.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-import Board from './Board.jsx'
+import React, { useState } from 'react'
 
 import ChessGame from '../../model'
+import { indexOf } from '../../model/utils.js'
+import { imageFromPiece } from './utils.js'
 
 export default function App() {
-  window.game = new ChessGame({})
-  return <Board game={window.game} />
+  const [game, setGame] = useState(new ChessGame({}))
+  window.game = game
+  const [selected, setSelected] = useState(null)
+
+  const [ranks] = useState([8, 7, 6, 5, 4, 3, 2, 1])
+  const [files] = useState(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+
+  const boardStyle = {
+    height: '100vh',
+    width: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  }
+
+  const rowStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row'
+  }
+
+  const getSquareStyle = function (rank, file, piece) {
+    return {
+      flex: 1,
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      backgroundImage: imageFromPiece[piece],
+      backgroundColor: (rank + file) % 2 ? 'brown' : 'beige',
+    }
+  }
+
+  return <div style={boardStyle}>
+    {ranks.map((rank, ri) =>
+      <div key={rank} style={rowStyle}>
+        {files.map((file, fi) =>
+          <div
+            key={file}
+            style={getSquareStyle(ri, fi, game.board[indexOf(ri, fi)])}
+            onMouseDown={() => { setSelected(indexOf(ri, fi)) }}
+            onMouseUp={() => {
+              setSelected(null)
+              const to = indexOf(ri, fi)
+              const move = game.getMoves().moves.filter(move => move.from === selected && move.to === to).pop()
+
+              if (move) setGame(game.makeMove(move))
+            }}
+          />
+        )}
+      </div>
+    )}
+  </div>
 }
