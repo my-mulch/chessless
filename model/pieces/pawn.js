e2016da18532d2a83ab71e63f1211d8735b0af74
trumanpurnell <truman.purnell@gmail.com>
2021-03-15 18:29:59 -0600

    moves for white are correct!

--- model/pieces/pawn.js
+++ model/pieces/pawn.js
@@ -42,8 +42,8 @@ export default class Pawn extends ChessPiece {
 
     getEnpassant(game, square, checkSquare, captureSquare) {
         const result = { moves: [], attacks: new Set(), checks: false }
-        
-        if (game.outOfBounds(checkSquare) || game.outOfBounds(captureSquare))
+
+        if (game.isOutOfBounds(checkSquare) || game.isOutOfBounds(captureSquare))
             return result
 
         const enpassant = {
@@ -62,8 +62,13 @@ export default class Pawn extends ChessPiece {
     // Capture
     getCapture(game, square, seekingCheck, captureSquare) {
         const result = { moves: [], attacks: new Set(), checks: false }
-        const captureMove = { from: square, to: captureSquare, piece: game.board[square] }
 
+        if (game.isOutOfBounds(captureSquare))
+            return result
+
+        result.attacks.add(captureSquare)
+
+        const captureMove = { from: square, to: captureSquare, piece: game.board[square] }
         if (!game.isOtherTeam(captureSquare, this) || (!seekingCheck && game.movePutsKingInCheck(captureMove)))
             return result
 
@@ -86,6 +91,14 @@ export default class Pawn extends ChessPiece {
             { from: square, to, piece, special(board) { board[to] = new Knight(super.getTeam(), piece.id) } },
             { from: square, to, piece, special(board) { board[to] = new Bishop(super.getTeam(), piece.id) } },
         )
+
+        // Capture & promote
+        if (Math.abs(square - to) === 7 || Math.abs(square - to) === 9) {
+            result.attacks(to)
+
+            if (game.board[to].getType() === ChessPiece.KING)
+                result.checks = true
+        }
 
         return result
     }
